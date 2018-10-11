9d2234e9c1e44f73202a37732ef51152edab1b52
unknown <2123162@labinfo.is.escuelaing.edu.co>
2018-10-11 16:16:29 -0500

    primer commit

---
+++ src/main/java/com/parcial/mercado/MercadoApplication.java
@@ -0,0 +1,12 @@
+package com.parcial.mercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoApplication.class, args);
+	}
+}
